c372516c3e1c03a1d0167626f840d81ac69fd12a
Cooper Baker <cooperbaker@gmail.com>
2024-02-16 10:23:07 -0800

    add lowpass

--- python/MCP3208.py
+++ python/MCP3208.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# mcp3208.py
+# MCP3208.py
 # mcp3208 spi reader for raspberry pi
 #
 # Cooper Baker (c) 2024
@@ -15,27 +15,34 @@ import spidev
 #-------------------------------------------------------------------------------
 class mcp3208():
 
-    # locals
+    # spi
     spi    = spidev.SpiDev()
     bus    = 0
     device = 0
+    value  = [ 0 ] * 8
+
+    # lowpass filter
+    y      = [ 0 ] * 8
+    a      = 0.05 # 0 to 1 : larger = less lag
 
     # constructor
     def __init__( self, bus, device ):
         self.bus = bus
         self.device = device
-        self.spi.open( self.bus, self.device )
-        self.spi.max_speed_hz = 100000
-        self.spi.mode = 0
-        self.spi.close()
+
+    def read( self ):
+        for i in range( 8 ):
+            self.value[ i ] = self.read_ch( i )
 
     # read the value of a channel
-    def read( self, channel ):
+    def read_ch( self, channel ):
         # mask 3 channel bits
         channel &= 0b00000111
 
         # get two bytes from the adc channel conversion registers
         self.spi.open( self.bus, self.device )
+        self.spi.max_speed_hz = 100000
+        self.spi.mode = 0
         adc = self.spi.xfer2( [ 6 | ( channel & 4 ) >> 2, ( channel & 3 ) << 6, 0 ] )
         self.spi.close()
 
@@ -43,10 +50,18 @@ class mcp3208():
         val = ( ( adc[ 1 ] & 15 ) << 8 ) + adc[ 2 ]
 
         # mask 12 bits of the value
-        return ( val & 0x0FFF )
+        val = val & 0x0FFF
+
+        return self.filter( channel, val )
+
+    # adc input filter
+    def filter( self, channel, x ):
+        # lowpass
+        self.y[ channel ] = self.a * x + ( 1 - self.a ) * self.y[ channel ]
+        return self.y[ channel ]
 
     # destructor
-    def cleanup():
+    def cleanup( self ):
         self.spi.close()
 
 #-------------------------------------------------------------------------------
