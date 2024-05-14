a095f3b3d866908f69db9f8f662a387ed96ff46c
Cooper Baker <cooperbaker@gmail.com>
2024-05-14 12:55:02 -0700

    pass callbacks to constructors

--- python/skronk.py
+++ python/skronk.py
@@ -10,157 +10,133 @@
 # imports
 #-------------------------------------------------------------------------------
 import time
-
-from RPLCD.i2c            import CharLCD
-from SKRONK.PINS          import *
-from SKRONK.SWITCH        import switch
-from SKRONK.MCP3208       import mcp3208
-from SKRONK.ENCODER       import encoder
-from SKRONK.LAN_IP        import lan_ip
-from SKRONK.OSC           import osc_io
-from SKRONK.THREAD        import thread
+from SKRONK.PINS    import *
+from SKRONK.LCD     import lcd_disp
+from SKRONK.SWITCH  import switch
+from SKRONK.MCP3208 import mcp3208
+from SKRONK.ENCODER import encoder
+from SKRONK.LAN_IP  import lan_ip
+from SKRONK.OSC     import osc_io
+from SKRONK.THREAD  import thread
 
 
 #-------------------------------------------------------------------------------
-# open sound control
+# open sound control config
 #-------------------------------------------------------------------------------
-
 # network
 OSC_IN_IP    = lan_ip()
 OSC_IN_PORT  = 1000
 OSC_OUT_IP   = '10.0.0.3'
 OSC_OUT_PORT = 1001
 
-# message addresses
-OSC_ENC = '/enc/'
-OSC_SW  = '/sw/'
-OSC_ADC = '/adc/'
+# internal
+# OSC_IN_IP    = '127.0.0.1'
+# OSC_IN_PORT  = 1000
+# OSC_OUT_IP   = '127.0.0.1'
+# OSC_OUT_PORT = 1001
+
+# input addresses
 OSC_LCD = '/lcd'
 OSC_CMD = '/cmd'
 
-# instantiate osc server
-osc = osc_io( OSC_IN_IP, OSC_IN_PORT, OSC_OUT_IP, OSC_OUT_PORT )
-
-# define message parse callback
-def osc_parse( address, *args ):
-
-    if address == OSC_LCD:
-        lcd.home()
-        lcd.write_string( str( args[ 0 ] ) )
-        print( OSC_LCD + ' ' + str( args[ 0 ] ) )
-
-    elif address == OSC_CMD:
-        print( 'cmd' )
-
-    elif address:
-        print( f'{ address }: { args }' )
-
-# assign message parse callback
-osc.set_parse( osc_parse )
+# output addresses
+OSC_ENC = '/enc/'
+OSC_SW  = '/sw/'
+OSC_ADC = '/adc/'
 
 
 #-------------------------------------------------------------------------------
-# run flag
+# lcd - for PCF8574 Family
 #-------------------------------------------------------------------------------
-run = True
-
-
-#-------------------------------------------------------------------------------
-# lcd
-#-------------------------------------------------------------------------------
-lcd = CharLCD( i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dotsize=8 )
+lcd = lcd_disp( 20, 4, 15 )
 lcd.clear()
-lcd.write_string( lan_ip() )
+lcd.write( lan_ip() )
+lcd.write_xy( 16, 2, ':)' )
 
 
 #-------------------------------------------------------------------------------
-# adcs
+# open sound control
 #-------------------------------------------------------------------------------
-# make adc objects: mcp3208( i2s_bus, i2s_device )
-adc1 = mcp3208( 0, 0 )
-adc2 = mcp3208( 0, 1 )
+# define osc message callback
+def osc_message( address, *args ):
+    if address == OSC_LCD:
+        lcd.write( str( args[ 0 ] ) )
+    elif address == OSC_CMD:
+        if args[ 0 ] == 'lcd_off':
+            lcd.off()
+        elif args[ 0 ] == 'lcd_on':
+            lcd.on()
 
-# define change callbacks
-def adc1_change( channel, value ):
-    osc.send( OSC_ADC + str( channel ), value )
-    # lcd.home()
-    # lcd.write_string( 'ADC ' + str( channel ) + ' ' + str( value ) + '    ' )
-    # print( 'ADC ' + str( channel ) + ' : ' + str( value ) )
-
-def adc2_change( channel, value ):
-    osc.send( OSC_ADC + str( channel + 8 ), value )
-    # lcd.home()
-    # lcd.write_string( 'ADC ' + str( channel + 8 ) + ' ' + str( value )  + '    ' )
-    # print( 'ADC ' + str( channel + 8 ) + ' : ' + str( value ) )
-
-
-# assign callbacks
-adc1.change = adc1_change
-adc2.change = adc2_change
-
-# define adc read callback
-def adc_read():
-    adc1.read()
-    adc2.read()
-
-# start adc read thread
-adc_thread = thread( adc_read, 1 )
+# make osc server: osc_io( in_ip, in_port, out_ip, out_port, message_callback )
+osc = osc_io( OSC_IN_IP, OSC_IN_PORT, OSC_OUT_IP, OSC_OUT_PORT, osc_message )
 
 
 #-------------------------------------------------------------------------------
 # switches
 #-------------------------------------------------------------------------------
-# make switch object: switch( [ pin_numbers ] )
-sw = switch( [ S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12 ] )
+# define change handler callback
+def sw_change( channel, state ):
+    osc.send( OSC_SW + str( channel ), state )
 
-# define on/off callbacks
-def sw_on( channel ):
-    osc.send( OSC_SW + str( channel ), 1 )
+# make switch object: switch( [ pin_numbers ], change_callback )
+sw = switch( [ S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12 ], sw_change )
 
-def sw_off( channel ):
-    osc.send( OSC_SW + str( channel ), 0 )
-
-# assign callbacks
-sw.on  = sw_on
-sw.off = sw_off
-
-# start switch read thread
+# start switch read thread at 1 msec interval
 sw_thread = thread( sw.read, 1 )
 
 
 #-------------------------------------------------------------------------------
 # encoders
 #-------------------------------------------------------------------------------
-# make encoder objects: encoder( pin_a, pin_b )
-enc1 = encoder( E1A, E1B )
-enc2 = encoder( E2A, E2B )
+# define change callbacks
+def enc_change( channel, direction ):
+    osc.send( OSC_ENC + str( channel ), direction )
 
-# define inc/dec callbacks
-def enc1_inc():
-    osc.send( OSC_ENC + '1', 1 )
+def enc1_change( direction ):
+    enc_change( 1, direction )
 
-def enc1_dec():
-    osc.send( OSC_ENC + '1', 0 )
+def enc2_change( direction ):
+    enc_change( 2, direction )
 
-def enc2_inc():
-    osc.send( OSC_ENC + '2', 1 )
+# make encoder objects: encoder( pin_a, pin_b, change_callback )
+enc1 = encoder( E1A, E1B, enc1_change )
+enc2 = encoder( E2A, E2B, enc2_change )
 
-def enc2_dec():
-    osc.send( OSC_ENC + '2', 0 )
-
-# assign callbacks
-enc1.inc = enc1_inc
-enc1.dec = enc1_dec
-enc2.inc = enc2_inc
-enc2.dec = enc2_dec
-
-# define encoder read callback
+# define read callback for threading
 def enc_read():
     enc1.read()
     enc2.read()
 
-# start encoder read thread
+# start encoder read thread at 1 msec interval
 enc_thread = thread( enc_read, 1 )
+
+
+#-------------------------------------------------------------------------------
+# adcs
+#-------------------------------------------------------------------------------
+# define change callbacks
+def adc_change( channel, value ):
+    osc.send( OSC_ADC + str( channel ), value )
+    lcd.write_xy( 8, 3, '     ' )
+    lcd.write_xy( 8, 3, str( channel ) + '~' + str( value ) )
+
+def adc1_change( channel, value ):
+    adc_change( channel, value )
+
+def adc2_change( channel, value ):
+    adc_change( channel + 8, value )
+
+# make adc objects: mcp3208( i2s_bus, i2s_device, change_callback )
+adc1 = mcp3208( 0, 0, adc1_change )
+adc2 = mcp3208( 0, 1, adc2_change )
+
+# define adc read callback for threading
+def adc_read():
+    adc1.read()
+    adc2.read()
+
+# start adc read thread at 1 msec interval
+adc_thread = thread( adc_read, 1 )
 
 
 #-------------------------------------------------------------------------------
@@ -168,12 +144,17 @@ enc_thread = thread( enc_read, 1 )
 #-------------------------------------------------------------------------------
 def main():
 
-    while run:
-        # lcd.home()
-        # lcd.write_string( str( round( adc1.value[ 0 ], 3 ) ) )
+    print( ' \n' )
+    print( 'Skronk Hat' )
+    print( ' \n' )
+    print( 'ip : ' + lan_ip() )
+    print( ' \n' )
+
+    while True:
         time.sleep( 0.1 )
 
 main()
+
 
 #-------------------------------------------------------------------------------
 # eof
