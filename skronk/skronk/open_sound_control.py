39bb0b9923937b3b943e65d2b2551724f54b0b0f
Cooper Baker <cooperbaker@gmail.com>
2024-07-24 09:38:21 -0700

    clean up

--- skronk/skronk/open_sound_control.py
+++ skronk/skronk/open_sound_control.py
@@ -24,21 +24,20 @@ class open_sound_control():
     def __init__( self, in_ip, in_port, out_ip, out_port, message_callback ):
 
         # network info
-        self.out_ip     = out_ip
-        self.out_port   = out_port
-        self.in_ip      = in_ip
-        self.in_port    = in_port
-        self.message    = message_callback
+        self.out_ip   = out_ip
+        self.out_port = out_port
+        self.in_ip    = in_ip
+        self.in_port  = in_port
+        self.message  = message_callback
 
         # client
-        self.client     = udp_client.SimpleUDPClient( self.out_ip, self.out_port )
+        self.client = udp_client.SimpleUDPClient( self.out_ip, self.out_port )
 
         # server
         self.dispatcher = Dispatcher()
         self.dispatcher.set_default_handler( self.message )
         self.server     = osc_server.ThreadingOSCUDPServer( ( self.in_ip, self.in_port ), self.dispatcher )
-        self.thread     = threading.Thread( target = self.server.serve_forever )
-        self.thread.start()
+        self.thread     = threading.Thread( target = self.server.serve_forever ).start()
 
     # send a message
     def send( self, addr, val ):
