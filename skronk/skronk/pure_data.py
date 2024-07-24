96acdbf57678fa6b37f445f620a00eaa5e4efb20
Cooper Baker <cooperbaker@gmail.com>
2024-07-24 09:37:42 -0700

    clean up class

--- skronk/skronk/pure_data.py
+++ skronk/skronk/pure_data.py
@@ -1,45 +1,25 @@
 import os
-import subprocess
-import signal
-
-
+from signal import SIGTERM
+from subprocess import Popen
+from pathlib import Path
 
 class pure_data():
 
-    def __init__( self, path ):
-        self.path = path
-        self.file = ''
-        self.list = []
-        self.proc = 0
+    def __init__( self ):
+        self.proc = False
 
-    def ls( self ):
-        self.list = os.listdir( self.path )
-        print( self.list )
-
-    def run( self, file ):
-        self.file = file
-        # self.proc = subprocess.Popen( 'sudo /usr/bin/pd -nogui -alsa ' + self.path + '/'+ self.file, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # self.proc = subprocess.Popen( 'sudo /usr/bin/pd -nogui -alsa ' + self.path + '/'+ self.file, shell=True, stdin=subprocess.PIPE )
-        # self.proc = subprocess.Popen( 'sudo /usr/bin/pd -nogui -alsa ', shell=True, stdin=subprocess.PIPE )
-
-        self.proc = subprocess.Popen( 'sudo /usr/bin/pd -nogui -alsa ' + self.path + '/'+ self.file, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid )
-
-
-        # self.proc = subprocess.Popen( 'sudo /usr/bin/pd -nogui -alsa ' + self.path + '/'+ self.file )
+    def run( self, path ):
+        if self.proc == False:
+            # note: must run as root
+            self.proc = Popen( [ '/usr/bin/pd', '-alsa', '-blocksize', '64', '-sleepgrain', '0', '-realtime', '-nogui', '-path', str( Path( path ).parent.resolve() ), '-open', path ], preexec_fn=os.setsid )
 
     def stop( self ):
+        try:
+            os.kill( self.proc.pid, SIGTERM )
+            os.killpg( os.getpgid( self.proc.pid ), SIGTERM )
+            self.proc.terminate()
+            self.proc.wait()
+            self.proc = False
+        except:
+            None
 
-### ??? HOW TO QUIT ? KILL ? UNLOAD
-
-        os.killpg( os.getpgid( self.proc.pid ), signal.SIGTERM )
-
-        self.proc.terminate()
-
-        self.proc.kill()
-
-        # os.killpg(os.getpgid( self.proc.pid), signal.SIGTERM)
-        # self.proc.terminate()
-        # self.proc.kill()
-
-        # self.proc.send_signal( 15 )
-        # self.proc.terminate()
