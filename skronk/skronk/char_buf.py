73e97844470bd53dce7dba25cb6b00345b56aef8
Cooper Baker <cooperbaker@gmail.com>
2024-08-19 11:08:28 -0700

    comment

--- skronk/skronk/char_buf.py
+++ skronk/skronk/char_buf.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # char_buf.py
-# Character buffer
+# 2D character buffer
 #
 # Cooper Baker (c) 2024
 #-------------------------------------------------------------------------------
@@ -18,11 +18,11 @@ class char_buf():
         self.size   = self.cols * self.rows
         self.buffer = ' ' * self.size
 
-    # clear - fill the character buffer with spaces
+    # clear - fill buffer with spaces
     def clear( self ):
         self.buffer = ' ' * self.size
 
-    # write - write a string into the character buffer at ( col, row ) location
+    # write - write string into buffer at ( col, row ) location
     def write( self, col, row, string ):
         self.buffer = self.buffer[ 0 : col + row * self.cols ] + string + self.buffer[ col + row * self.cols + len( string ) : self.size ]
 
