67562456efae09315f05f4adfb7de0591144131b
Cooper Baker <cooperbaker@gmail.com>
2024-08-19 11:05:53 -0700

    add device name lookup

--- skronk/skronk/pure_data.py
+++ skronk/skronk/pure_data.py
@@ -1,18 +1,29 @@
 #-------------------------------------------------------------------------------
 # pure_data.py
-# Pure Data subprocess
+# Pure Data interface object
 #
 # Cooper Baker (c) 2024
+#
+# pylint: disable = bare-except
+# pylint: disable = subprocess-popen-preexec-fn
 #-------------------------------------------------------------------------------
 
 
 #-------------------------------------------------------------------------------
 # imports
 #-------------------------------------------------------------------------------
-import os
+from os         import getpgid, kill, killpg, setsid
 from signal     import SIGTERM
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from pathlib    import Path
+
+
+#-------------------------------------------------------------------------------
+# audio device
+#-------------------------------------------------------------------------------
+# change this to match your system - list devices with the command:
+# $ pd -stderr -listdev -nogui -send "pd quit"
+AUDIO_DEVICE = 'USB Audio CODEC'
 
 
 #-------------------------------------------------------------------------------
@@ -26,20 +37,39 @@ class pure_data():
 
     # run - starts pd subprocess with patch at path
     def run( self, path ):
-        if self.proc == False:
+        if self.proc is False:
             # note: must run as root
-            self.proc = Popen( [ '/usr/bin/pd', '-alsa', '-blocksize', '64', '-sleepgrain', '0', '-realtime', '-nogui', '-path', str( Path( path ).parent.resolve() ), '-open', path ], preexec_fn=os.setsid )
+            self.proc = Popen( [ '/usr/bin/pd',
+                                 '-alsa',
+                                 '-audiodev', self.dev_id( AUDIO_DEVICE ),
+                                 '-blocksize', '64',
+                                 '-sleepgrain', '1',
+                                 '-realtime',
+                                 '-nogui',
+                                 '-path', str( Path( path ).parent.resolve() ),
+                                 '-open', path ],
+                                 preexec_fn = setsid )
+
+    # dev_id - returns first id number of name from "pd -listdev", or 0
+    def dev_id( self, name ):
+        cmd = Popen( [ 'pd', '-stderr', '-listdev', '-nogui', '-send', 'pd quit' ], stderr = PIPE, text = True )
+        out, err = cmd.communicate()
+        err = err.split( '\n' )
+        for line in err:
+            if line.find( name ) > -1 :
+                return str( line[ 0 ] )
+        return '0'
 
     # stop - terminates pd subprocess
     def stop( self ):
         try:
-            os.kill( self.proc.pid, SIGTERM )
-            os.killpg( os.getpgid( self.proc.pid ), SIGTERM )
+            kill( self.proc.pid, SIGTERM )
+            killpg( getpgid( self.proc.pid ), SIGTERM )
             self.proc.terminate()
             self.proc.wait()
             self.proc = False
         except:
-            None
+            pass
 
 
 #-------------------------------------------------------------------------------
