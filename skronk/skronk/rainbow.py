c7ce52bd32d07375c674a8daf2cb34c594a72ded
Cooper Baker <cooperbaker@gmail.com>
2024-08-30 10:38:15 -0700

    add patch list, load, stop, off

--- skronk/skronk/rainbow.py
+++ skronk/skronk/rainbow.py
@@ -12,9 +12,7 @@
 # imports
 #-------------------------------------------------------------------------------
 from urllib.request import urlopen
-from json import loads
-from os import system
-
+from json           import loads
 
 #-------------------------------------------------------------------------------
 # rainbow class
@@ -22,14 +20,25 @@ from os import system
 class rainbow():
 
     # constructor
-    def __init__( self ):
+    def __init__( self, osc ):
+
+        self.osc = osc
+
+        # local rnbo @ http://127.0.0.1:5678
+        self.ip = '127.0.0.1'
+        self.port = 5678
+        self.url = 'http://' + self.ip + ':' + str( self.port )
 
         # patch names
-        self.patch = []
-        self.active = False
+        self.patch   = []
+        self.patches = 0
+        self.current_patch = ''
 
-    # osc_message - turn rnbo @meta {'osc':'/messages'} into normal osc messages
-    def osc_message( self, address, *args ):
+        # tell rnbo skronk is listening so rnbo will send osc messages
+        osc.send( '/rnbo/listeners/add', osc.in_ip + ':' + str( osc.in_port ) )
+
+    # osc_format - turn rnbo @meta {'osc':'/messages'} into normal osc messages
+    def osc_format( self, address, *args ):
         rnbo_meta = address.split( ' ' )
         if len( rnbo_meta ) > 1 :
             address = rnbo_meta[ 0 ]
@@ -39,28 +48,50 @@ class rainbow():
                 args = rnbo_meta[ 1 : ]
         return address, *args
 
-    # get_patchers - get names of available patchers and update self.active flag
-    def get_patchers( self ):
-        try:
-            json = loads( urlopen( 'http://127.0.0.1:5678/rnbo/patchers' ).read() )
-            self.patch = []
+    # ls - fill self.patch[] with names of available patches
+    def ls( self ):
+        self.patches = 0
+        self.patch   = []
+        try :
+            json = loads( urlopen( self.url + '/rnbo/patchers' ).read() )
             for name in json[ 'CONTENTS' ]:
                 self.patch.append( name )
-            self.active = True
+            self.patch.sort( key = lambda x:x[ 0 ] )
+            self.patches = len( self.patch )
+            while self.patches < 4 :
+                self.patch.append( '' )
+                self.patches = self.patches + 1
+        except :
+            self.patch = []
+            self.patches = 0
+
+    # current_patch - put current patch name in self.current_patch
+    def get_current_patch( self ):
+        try :
+            json = loads( urlopen( self.url + '/rnbo/inst/0/name' ).read() )
+            self.current_patch = json[ 'VALUE' ]
         except:
-            self.active = False
-            print( 'error: get_patchers() - rnbo is not responding... ' )
+            self.current_patch = ''
+
+    # load - load a patch ( activates jack if inactive )
+    def load( self, patch ):
+        self.osc.send( '/rnbo/inst/control/load', [ 0, patch ]  )
+        self.current_patch = patch
+
+    # stop - unload a patch
+    def stop( self ):
+        self.osc.send( '/rnbo/inst/control/unload', [ 'i', 0 ] )
+
+    # off - deactivate jack
+    def off( self ):
+        self.osc.send( '/rnbo/jack/active', 0 )
 
 
 #-------------------------------------------------------------------------------
 # to do ...
 #-------------------------------------------------------------------------------
-
-    # def load( self, name ):
-        # osc.send( '/rnbo/inst/control/unload', [ 'i', -1 ] )
-
-    # def unload( self, patch ):
-        # osc.send( '/rnbo/inst/control/load', [ 0, patch ]  )
+    # def on( self ):
+    #     self.osc.send( '/rnbo/jack/active', 1 )
 
 
     # def device( self, name ):
@@ -71,13 +102,7 @@ class rainbow():
     # def get_devices( self ):
 
 
-    # def stop( self ):
-    #     system( 'sudo systemctl stop rnbooscquery.service' )
-
-    # def start( self ):
-        # system( 'sudo systemctl start rnbooscquery.service' )
-
-
 #-------------------------------------------------------------------------------
 # eof
 #-------------------------------------------------------------------------------
+
