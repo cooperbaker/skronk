d0268f2ac64bc58e1ff24fdef8735eb76ddd3d07
Cooper Baker <cooperbaker@gmail.com>
2024-08-30 10:38:55 -0700

    add page management

--- skronk/skronk/menu.py
+++ skronk/skronk/menu.py
@@ -1,55 +1,97 @@
-from skronk.char_buf import char_buf
+#-------------------------------------------------------------------------------
+# menu.py
+# Menu object for skronk
+#
+# Cooper Baker (c) 2024
+#-------------------------------------------------------------------------------
 
-class page():
 
-    def __init__( self ):
-        self.buffer = char_buf( 20, 4 )
-        self.draw()
+#-------------------------------------------------------------------------------
+# imports
+#-------------------------------------------------------------------------------
+from .thread     import thread
+from .menus.pd   import pd
+from .menus.rnbo import rnbo
+from .menus.stat import stat
 
-    def draw( self ):
-        self.buffer.write( 0, 0, '- Menu Page --------' )
-        self.buffer.write( 0, 1, '--------------------' )
-        self.buffer.write( 0, 2, '--------------------' )
-        self.buffer.write( 0, 3, '--------------------' )
 
-    # switch event callbacks
-    def a( self ):
-        pass
-    def b( self ):
-        pass
-    def c( self ):
-        pass
-    def d( self ):
-        pass
-    def e( self ):
-        pass
-    def f( self ):
-        pass
-    def g( self ):
-        pass
-    def h( self ):
-        pass
-
-    # sw_event - select switch events and run callbacks
-    def sw_event( self, channel, value ):
-        if ( channel == 1 ) and value :
-            self.a()
-        if ( channel == 2 ) and value :
-            self.b()
-        if ( channel == 3 ) and value :
-            self.c()
-        if ( channel == 4 ) and value :
-            self.d()
-        if ( channel == 5 ) and value :
-            self.e()
-        if ( channel == 6 ) and value :
-            self.f()
-        if ( channel == 7 ) and value :
-            self.g()
-        if ( channel == 8 ) and value :
-            self.h()
-
+#-------------------------------------------------------------------------------
+# menu class
+#-------------------------------------------------------------------------------
 class menu():
 
-    def __init__( self ):
-        self.main = page()
+    # toggle switch for menu activation
+    MENU_SWITCH = 17
+
+    # tick rate
+    TICK_MSEC = 333.333
+
+    # constructor
+    def __init__( self, skronk ):
+
+        # store this object in the skronk object
+        skronk.menu = self
+
+        # store the skronk object
+        self.skronk = skronk
+
+        # create pages
+        self.stat = stat( skronk )
+        self.pd   = pd  ( skronk )
+        self.rnbo = rnbo( skronk )
+
+        # set up page navigation: nav( up, down, left, right )
+        self.stat.nav( None, None, self.rnbo, self.pd   )
+        self.pd.nav  ( None, None, self.stat, self.rnbo )
+        self.rnbo.nav( None, None, self.pd,   self.stat )
+
+        # current active page
+        self.page = self.stat
+
+        # visible flag
+        self.visible = 0
+
+        # page tick() thread
+        self.tick_thread = thread( self.tick, self.TICK_MSEC )
+
+    # tick - threaded callback to run visible page tick() method
+    def tick( self ):
+        if self.visible :
+            self.page.tick()
+
+    # stop - stop the tick thread
+    def stop( self ):
+        self.tick_thread.stop()
+
+    # sw_event - switch event callback handler
+    def sw_event( self, channel, value ):
+        if ( channel == self.MENU_SWITCH ) and value :
+            self.toggle()
+        # only give events to page if menu is visible
+        if self.visible and ( channel != self.MENU_SWITCH ) :
+            self.page.sw_event( channel, value )
+
+    # toggle - activate / deactivate the page
+    def toggle( self ):
+        # grip it and flip it
+        self.visible = not( self.visible )
+        if self.visible :
+            self.page.tick()
+            self.page.draw()
+            self.skronk.disp.set_buffer( self.page.buffer )
+        else :
+            self.skronk.disp.set_buffer( self.skronk.disp.buffer )
+
+    # set_page - set the active page
+    def set_page( self, page ):
+        if page :
+            self.page = page
+            if self.visible :
+                self.page.tick()
+                self.page.draw()
+                self.skronk.disp.set_buffer( self.page.buffer )
+
+
+#-------------------------------------------------------------------------------
+# eof
+#-------------------------------------------------------------------------------
