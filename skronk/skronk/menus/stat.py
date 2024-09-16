a8caa8da0863bb5c94aa001c84315701a08ea776
Cooper Baker <cooperbaker@gmail.com>
2024-09-16 10:52:51 -0700

    remove patch

--- skronk/skronk/menus/stat.py
+++ skronk/skronk/menus/stat.py
@@ -1,6 +1,7 @@
 #-------------------------------------------------------------------------------
 # stat.py
 # Menu page for skronk
+#
 # displays cpu percent, mem percent, cpu temperature
 #
 # Cooper Baker (c) 2024
@@ -31,11 +32,11 @@ class stat( page ):
         self.clear()
                         #  ....................
                         #  STAT____________<12>
-                        #  patch.filename......
+                        #  ....................
                         #  cpu 99.9%.temp.99.9c
                         #  mem 99.9%...........
         self.write( 0, 0, 'STAT____________\x11\x01\x02\x10' )
-        self.write( 0, 1, 'patch               ' )
+        self.write( 0, 1, '                    ' )
         self.write( 0, 2, f'cpu { self.skronk.cpu() }% temp { self.skronk.temp() }c' )
         self.write( 0, 3, f'mem { self.skronk.mem() }%           ' )
         self.display()
