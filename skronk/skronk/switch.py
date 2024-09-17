350ffa51144da0f97bee189f4e27327e6f472c4f
Cooper Baker <cooperbaker@gmail.com>
2024-09-16 19:34:25 -0700

    add pins and callback

--- skronk/skronk/switch.py
+++ skronk/skronk/switch.py
@@ -21,16 +21,33 @@ class switch():
     # class-wide static gpio interface object
     gpio = Chip( 'gpiochip4', Chip.OPEN_BY_NAME )
 
+    # switch pins
+    S1  = 5
+    S2  = 25
+    S3  = 6
+    S4  = 24
+    S5  = 12
+    S6  = 23
+    S7  = 13
+    S8  = 22
+    S9  = 19
+    S10 = 27
+    S11 = 16
+    S12 = 18
+    S13 = 26
+    S14 = 17
+    S15 = 20
+    S16 = 4
+    S17 = 21
+
+    pin = [ S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15, S16, S17 ]
+
     # constructor
-    # def callback( channel, state )
-    def __init__( self, pins, callback ):
+    def __init__( self ):
         self.line     = []
+        self.state    = []
         self.value    = []
         self.prev     = []
-        self.pin      = pins
-        self.callback = callback
-
-        self.state = []
 
         # set up gpio line objects
         for i, pin in enumerate( self.pin ):
@@ -61,6 +78,9 @@ class switch():
                 self.callback( i + 1, 0 )
                 self.prev[ i ] = self.value[ i ]
 
+    # callback - event callback
+    def callback( self, channel, value ):
+        pass
 
 #-------------------------------------------------------------------------------
 # eof
