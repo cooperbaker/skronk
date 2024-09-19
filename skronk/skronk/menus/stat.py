9c5c3ab0f694f6610582de389cf9f80d32cf9ad8
Cooper Baker <cooperbaker@gmail.com>
2024-09-19 15:09:08 -0700

    use utility.py

--- skronk/skronk/menus/stat.py
+++ skronk/skronk/menus/stat.py
@@ -12,7 +12,7 @@
 # imports
 #-------------------------------------------------------------------------------
 from .page import page
-
+from ..utility import cpu, mem, temp
 
 #-------------------------------------------------------------------------------
 # stat class
@@ -22,6 +22,8 @@ class stat( page ):
     # constructor
     def __init__( self, system ):
         super().__init__( system )
+
+        self.cpu = cpu()
 
     # tick callback
     def tick( self ):
@@ -37,8 +39,8 @@ class stat( page ):
                         #  mem 99.9%...........
         self.write( 0, 0, 'STAT____________\x11\x01\x02\x10' )
         self.write( 0, 1, '                    ' )
-        self.write( 0, 2, f'cpu { self.system.cpu() }% temp { self.system.temp() }c' )
-        self.write( 0, 3, f'mem { self.system.mem() }%           ' )
+        self.write( 0, 2, f'cpu { self.cpu.stat() }% temp { temp() }c' )
+        self.write( 0, 3, f'mem { mem() }%           ' )
         self.display()
 
     # sw1 - navigate left
