c8a386f8e5461759c8fb6de91502ebcafe52e9fe
Cooper Baker <cooperbaker@gmail.com>
2024-10-31 12:36:45 -0700

    comment

--- install.sh
+++ install.sh
@@ -4,27 +4,34 @@
 #
 # Skronk Install Script For RNBO Raspbian OS Image
 #
-# 1. Follow skronk sd card flashing instructons URL
-# 2. Run this raspberry pi command to install skronk:
-# $  curl --raw https://raw.githubusercontent.com/cooperbaker/skronk/refs/heads/main/install.sh | bash
+# 1. Follow skronk sd card flashing instructons in README.md
+# 2. Run this command from a fresh os image to install skronk:
+# $  curl https://raw.githubusercontent.com/cooperbaker/skronk/refs/heads/main/install.sh | bash
 #
 # Cooper Baker (c) 2024
 #-------------------------------------------------------------------------------
 
-# hello
+
+#-------------------------------------------------------------------------------
+# install skronk
+#-------------------------------------------------------------------------------
 sudo echo ""
-echo -e "\033[1mInstalling Skronk..."
+echo -e "\033[1mInstalling Skronk"
 echo -e "\033[0m\033[1A"
 echo ""
 
+#-------------------------------------------------------------------------------
 # update packages
+#-------------------------------------------------------------------------------
 echo -e "\033[1mUpdating Package Lists..."
 echo -e "\033[0m\033[1A"
 echo ""
 sudo apt -y update
 echo ""
 
+#-------------------------------------------------------------------------------
 # install packages
+#-------------------------------------------------------------------------------
 echo -e "\033[1mInstalling Packages..."
 echo -e "\033[0m\033[1A"
 echo ""
@@ -35,7 +42,9 @@ sudo apt -y install puredata
 sudo apt -y install git
 echo""
 
-# update everything and clean packages
+#-------------------------------------------------------------------------------
+# upgrade packages
+#-------------------------------------------------------------------------------
 echo -e "\033[1mUpgrading Packages..."
 echo -e "\033[0m\033[1A"
 echo ""
@@ -46,7 +55,9 @@ sudo apt -y autoremove
 sudo apt -y autoclean
 echo""
 
+#-------------------------------------------------------------------------------
 # install skronk library
+#-------------------------------------------------------------------------------
 echo -e "\033[1mInstalling Skronk Library..."
 echo -e "\033[0m\033[1A"
 echo ""
@@ -55,14 +66,10 @@ sudo rm -r skronk
 git clone --depth 1 https://github.com/cooperbaker/skronk
 chmod -v 755 ./skronk/skronk.py
 echo ""
-# # update
-# cd skronk
-# pwd
-# git pull
-# chmod -v 755 skronk.py
-# echo ""
 
-# install skronk-pd
+#-------------------------------------------------------------------------------
+# install skronk-pd library
+#-------------------------------------------------------------------------------
 # echo -e "\033[1mInstalling Skronk-Pd Library"
 # echo -e "\033[0m\033[1A"
 # echo ""
@@ -77,7 +84,9 @@ echo ""
 # cd ~
 # echo ""
 
+#-------------------------------------------------------------------------------
 # enable i2c and spi
+#-------------------------------------------------------------------------------
 echo -e "\033[1mEnabling I2C and SPI..."
 echo -e "\033[0m\033[1A"
 echo ""
@@ -87,25 +96,27 @@ echo "sudo raspi-config nonint do_spi 0"
 sudo raspi-config nonint do_spi 0
 echo ""
 
+#-------------------------------------------------------------------------------
 # create system service
+#-------------------------------------------------------------------------------
 echo -e "\033[1mCreating Skronk Service..."
 echo -e "\033[0m\033[1A"
 echo ""
 sudo systemctl disable skronk.service
 sudo ln -sv /home/pi/skronk/skronk/skronk.service /etc/systemd/system/skronk.service
 sudo systemctl enable skronk.service
+sudo systemctl start skronk.service
 echo ""
 
-# goodbye
+#-------------------------------------------------------------------------------
+# install complete
+#-------------------------------------------------------------------------------
 echo -e "\033[1mSkronk Install Complete"
 echo -e "\033[0m\033[1A"
 echo ""
 echo "Goodbye :)"
 echo ""
 
-# read -n 1 -r -s -p $'Press any key to reboot...\n'
-# sudo reboot now
-# echo "reboot disabled"
 
 #-------------------------------------------------------------------------------
 # eof
