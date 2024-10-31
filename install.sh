d38dda30f36c9308237a8acae3885e13f6170caf
Cooper Baker <cooperbaker@gmail.com>
2024-10-31 10:56:13 -0700

    edit

--- install.sh
+++ install.sh
@@ -1,58 +1,58 @@
-#!/bin/bash
-#-------------------------------------------------------------------------------
-# install.sh
-#
-# Skronk Install Script For RNBO Raspbian OS Image
-#
-# 1. Follow skronk sd card flashing instructons URL
-# 2. Run this raspberry pi command to install skronk:
-# $  curl https://raw.githubusercontent.com/cooperbaker/skronk/refs/heads/main/install.sh | bash
-#
-# Cooper Baker (c) 2024
-#-------------------------------------------------------------------------------
+# #!/bin/bash
+# #-------------------------------------------------------------------------------
+# # install.sh
+# #
+# # Skronk Install Script For RNBO Raspbian OS Image
+# #
+# # 1. Follow skronk sd card flashing instructons URL
+# # 2. Run this raspberry pi command to install skronk:
+# # $  curl https://raw.githubusercontent.com/cooperbaker/skronk/refs/heads/main/install.sh | bash
+# #
+# # Cooper Baker (c) 2024
+# #-------------------------------------------------------------------------------
 
-# hello
-sudo echo ""
-echo -e "\033[1mInstalling Skronk"
-echo -e "\033[0m\033[1A"
-echo ""
+# # hello
+# sudo echo ""
+# echo -e "\033[1mInstalling Skronk"
+# echo -e "\033[0m\033[1A"
+# echo ""
 
-# update packages
-sudo apt -y update
+# # update packages
+# sudo apt -y update
 
-# install packages
-sudo apt -y install jack_transport_link
-sudo apt -y install rnbo-runner-panel
-sudo apt -y install python3-smbus
-sudo apt -y install puredata
-sudo apt -y install git
+# # install packages
+# sudo apt -y install jack_transport_link
+# sudo apt -y install rnbo-runner-panel
+# sudo apt -y install python3-smbus
+# sudo apt -y install puredata
+# sudo apt -y install git
 
-# update everything and clean packages
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y clean
-sudo apt -y autoremove
-sudo apt -y autoclean
+# # update everything and clean packages
+# sudo apt -y update
+# sudo apt -y upgrade
+# sudo apt -y clean
+# sudo apt -y autoremove
+# sudo apt -y autoclean
 
-# clone repositories
-cd ~
-git clone --depth 1 https://github.com/cooperbaker/skronk
-# git clone --depth 1 https://github.com/cooperbaker/skronk-pd pd
-echo ""
+# # clone repositories
+# cd ~
+# git clone --depth 1 https://github.com/cooperbaker/skronk
+# # git clone --depth 1 https://github.com/cooperbaker/skronk-pd pd
+# echo ""
 
-# update repositories
-cd ~
-cd skronk
-pwd
-git pull
-chmod -v 755 skronk.py
-echo ""
-cd ~
-cd pd
-pwd
-git pull
-cd ~
-echo ""
+# # update repositories
+# cd ~
+# cd skronk
+# pwd
+# git pull
+# chmod -v 755 skronk.py
+# echo ""
+# cd ~
+# cd pd
+# pwd
+# git pull
+# cd ~
+# echo ""
 
 # enable i2c and spi
 echo -e "\033[1mEnable I2C And SPI In The Raspi-Config Menus:"
@@ -66,23 +66,23 @@ echo ""
 sudo raspi-config
 echo ""
 
-# create system service
-echo -e "\033[1mCreating Skronk Service"
-echo -e "\033[0m\033[1A"
-sudo ln -sv /home/pi/skronk/skronk/skronk.service /etc/systemd/system/skronk.service
-sudo systemctl enable skronk.service
-echo ""
+# # create system service
+# echo -e "\033[1mCreating Skronk Service"
+# echo -e "\033[0m\033[1A"
+# sudo ln -sv /home/pi/skronk/skronk/skronk.service /etc/systemd/system/skronk.service
+# sudo systemctl enable skronk.service
+# echo ""
 
-# goodbye
-echo ""
-echo -e "\033[1mSkronk Install Complete"
-echo -e "\033[0m\033[1A"
-echo ""
+# # goodbye
+# echo ""
+# echo -e "\033[1mSkronk Install Complete"
+# echo -e "\033[0m\033[1A"
+# echo ""
 
-read -n 1 -r -s -p $'Press any key to reboot...\n'
-# sudo reboot now
-# echo "reboot disabled"
+# read -n 1 -r -s -p $'Press any key to reboot...\n'
+# # sudo reboot now
+# # echo "reboot disabled"
 
-#-------------------------------------------------------------------------------
-# eof
-#-------------------------------------------------------------------------------
+# #-------------------------------------------------------------------------------
+# # eof
+# #-------------------------------------------------------------------------------
